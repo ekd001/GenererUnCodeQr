5a0599cf28cde969125272623182ce278b603837
EKLOU Kossi Dodji <82749166+suzjehhejsjzj@users.noreply.github.com>
2023-08-25 12:30:40 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import  qrcode
+
+
+#message que vous voulez encoder dans un code QR
+message = input("votre message : ")
+
+#générer notre code QR en définissant la taille des pixels, le niveau d'erreur et la taille de la marge autour du code QR
+
+CodeQR = qrcode.QRCode(version=1,error_correction=qrcode.ERROR_CORRECT_L,box_size=10,border=4)
+CodeQR.add_data(message)
+CodeQR.make(fit=True)
+
+#créer une image du codeQR et l'enrégistrer
+img = CodeQR.make_image(fill_color="black",back_color="white")
+img.save("codeQR.png")
+
+
+
